91d00822b0aabfb7618d4bed1d9983f4ceb2ec34
Liza <gubanova-ea@yandex.ru>
2023-10-16 10:43:45 +0300

    Liza: show must go on

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SHOW MUST GO ON
         </a>
       </header>
     </div>
